aae7535e3edf50829fb5f837ae491529263f851a
Seal <will@kimbell.uk>
2025-02-10 13:17:26 +0000

    386   2025-02-09 12:37:31   (current)

---
+++ home/dotfilesbackup/hypr/theme/current.bash
@@ -0,0 +1,22 @@
+
+background='#1e1e2e99'
+foreground='#b4befe'
+color0='#1e1e2e'
+color1='#f38ba8'
+color2='#a6e3a1'
+color3='#f9e2af'
+color4='#89b4fa'
+color5='#cba6f7'
+color6='#94e2d5'
+color7='#b4befe'
+color8='#313244'
+color9='#f38ba8'
+color10='#a6e3a1'
+color11='#f9e2af'
+color12='#89b4fa'
+color13='#cba6f7'
+color14='#94e2d5'
+color15='#b4befe'
+
+# Wallpaper
+wallpaper="$HOME/.config/hypr/wallpapers/wallpaper.jpg"
