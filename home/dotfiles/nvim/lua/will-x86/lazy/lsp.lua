2678f9b265d86eda67c3eab3881d063729a05623
Seal <will@kimbell.uk>
2025-02-11 15:48:57 +0000

    454   2025-02-11 15:36:10   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
@@ -141,6 +141,24 @@ return {
                 "css", "graphql", "html", "javascript", "javascriptreact",
                 "json", "less", "scss", "typescript", "typescriptreact", "yaml",
             },
+            cli_options = {
+                arrow_parens = "always",
+                bracket_spacing = true,
+                bracket_same_line = false,
+                embedded_language_formatting = "auto",
+                end_of_line = "lf",
+                html_whitespace_sensitivity = "css",
+                jsx_bracket_same_line = false,
+                jsx_single_quote = false,
+                print_width = 80,
+                prose_wrap = "preserve",
+                quote_props = "as-needed",
+                semi = true,
+                single_quote = false,
+                tab_width = 2,
+                trailing_comma = "es5",
+                use_tabs = false,
+            },
         })
 
         -- Completion setup
