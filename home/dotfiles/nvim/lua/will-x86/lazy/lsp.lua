24d287d2ca028a2038f0da01ffb486b2749f3c02
Seal <will@kimbell.uk>
2025-02-11 11:26:48 +0000

    446   2025-02-11 11:25:28   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
@@ -14,18 +14,10 @@ return {
         "MunifTanjim/prettier.nvim",
     },
     config = function()
-        local lsp_zero = require('lsp-zero')
-        local cmp = require('cmp')
-        local nvim_lsp = require('lspconfig')
+        -- Reserve space in the gutter
+        vim.opt.signcolumn = 'yes'
 
-        -- Initialize lsp-zero
-        --lsp_zero.preset({}-
-        lsp_zero.preset({
-            name = 'minimal',
-            set_lsp_keymaps = true,
-            manage_nvim_cmp = true,
-            suggest_lsp_servers = false,
-        })
+        -- Add cmp_nvim_lsp capabilities to lspconfig
         local lspconfig_defaults = require('lspconfig').util.default_config
         lspconfig_defaults.capabilities = vim.tbl_deep_extend(
             'force',
@@ -35,22 +27,6 @@ return {
 
         -- Set up mason
         require("mason").setup({ PATH = "append" })
-
-        -- Format on save setup
-        local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-        local function lsp_format_on_save(bufnr)
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format()
-                    filter = function(client)
-                        return client.name == "null-ls"
-                    end
-                end,
-            })
-        end
 
         -- NixOS detection
         local is_nixos = (function()
@@ -73,32 +49,42 @@ return {
 
         require('mason-lspconfig').setup({
             ensure_installed = ensure_installed,
-            handlers = {
-                lsp_zero.default_setup,
-            },
         })
 
-        -- NixOS specific configurations
-        if is_nixos then
-            local nixos_configs = {
-                -- Your NixOS configurations here
-            }
-            for server, config in pairs(nixos_configs) do
-                require('lspconfig')[server].setup(config)
-            end
-        end
+        -- LSP attach configuration
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = { buffer = event.buf }
+
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "<C-o>", [[<Cmd>lua vim.cmd('normal! <C-O>')<CR>]], opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+                vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+                vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+            end,
+        })
+
+        -- Set up completion
+        local cmp = require('cmp')
         cmp.setup({
             snippet = {
                 expand = function(args)
                     require('luasnip').lsp_expand(args.body)
                 end,
             },
-            sources = cmp.config.sources({
+            sources = {
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' },
                 { name = 'buffer' },
                 { name = 'path' },
-            }),
+            },
             mapping = cmp.mapping.preset.insert({
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
                 ['<C-Space>'] = cmp.mapping.complete(),
@@ -112,58 +98,40 @@ return {
             })
         })
 
-        -- Set up command line completion
-        cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = 'path' },
-                { name = 'cmdline' }
-            })
-        })
-
         -- Disable completion for specific filetypes
         cmp.setup.filetype('sql', { enabled = false })
-        -- Configure sign icons
-        lsp_zero.set_sign_icons({
-            error = 'E',
-            warn = 'W',
-            hint = 'H',
-            info = 'I'
-        })
-
-        local disable_lsp_filetypes = { sql = true, mysql = true }
-
-        -- LSP attach configuration
-        lsp_zero.on_attach(function(client, bufnr)
-            local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-            if disable_lsp_filetypes[filetype] then
-                client.stop()
-                return
-            end
-
-            local opts = { buffer = bufnr, remap = false }
-            lsp_format_on_save(bufnr)
-
-            -- Your keymaps here
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "<C-o>", [[<Cmd>lua vim.cmd('normal! <C-O>')<CR>]], opts)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-            vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-            vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-            vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-            vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-            vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-        end)
-
-        -- Setup LSP
-        lsp_zero.setup()
 
         -- Configure diagnostics
         vim.diagnostic.config({
             virtual_text = true,
         })
+
+        -- Format on save setup
+        local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+        local function lsp_format_on_save(bufnr)
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format()
+                end,
+            })
+        end
+
+        -- Set up individual LSP servers
+        local lspconfig = require('lspconfig')
+
+        -- Configure servers based on OS
+        if is_nixos then
+            -- NixOS specific configurations
+            -- Add your NixOS-specific server configurations here
+        else
+            -- Non-NixOS configurations
+            for _, server in ipairs(ensure_installed) do
+                lspconfig[server].setup({})
+            end
+        end
     end
 }
+
