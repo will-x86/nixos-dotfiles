9200b600a546c4400308fa7b50c0fb976994663a
Seal <will@kimbell.uk>
2025-02-11 10:30:40 +0000

    430   2025-02-11 10:24:56   (current)

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -72,69 +72,55 @@ require("lazy").setup({
     },
     'MunifTanjim/prettier.nvim',
     {
-        "yetone/avante.nvim",
-        event = "VeryLazy",
-        lazy = false,
-        version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-
-        opts = {
-            provider = "claude",
-            claude = {
-                endpoint = "https://api.anthropic.com",
-                model = "claude-3-5-sonnet-latest",
-                temperature = 0,
-                max_tokens = 8192,
-                disable_tools = true, -- disable tools!
-            },
-            windows = {
-                width = 40,
-            },
-        },
-        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-        build = "make",
-        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        'yetone/avante.nvim',
         dependencies = {
-            "stevearc/dressing.nvim",
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            --- The below dependencies are optional,
-            "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-            "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-            "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-            "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+            'nvim-tree/nvim-web-devicons',
+            'stevearc/dressing.nvim',
+            'nvim-lua/plenary.nvim',
+            'MunifTanjim/nui.nvim',
             {
-                -- support for image pasting
-                "HakonHarnes/img-clip.nvim",
-                event = "VeryLazy",
-                opts = {
-                    -- recommended settings
-                    default = {
-                        embed_image_as_base64 = false,
-                        prompt_for_file_name = false,
-                        drag_and_drop = {
-                            insert_mode = true,
-                        },
-                        -- required for Windows users
-                        use_absolute_path = true,
-                    },
-                },
-            },
-            {
-                -- Make sure to set this up properly if you have lazy=true
-                'MeanderingProgrammer/render-markdown.nvim',
+                "MeanderingProgrammer/render-markdown.nvim",
                 opts = {
                     file_types = { "markdown", "Avante" },
                 },
                 ft = { "markdown", "Avante" },
-            },
+            }
         },
+        config = function()
+            require('avante').setup({
+                provider = "claude",
+                claude = {
+                    endpoint = "https://api.anthropic.com",
+                    model = "claude-3-5-sonnet-latest",
+                    temperature = 0,
+                    max_tokens = 8192,
+                },
+                windows = {
+                    width = 40,
+                },
+            })
+        end,
+        build = 'make', -- Optional, only if you want to use tiktoken_core to calculate tokens count
     },
+    'nvim-treesitter/playground',
     {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" }
 },
+    'folke/zen-mode.nvim',
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            'neovim/nvim-lspconfig',
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-nvim-lsp',
+            'L3MON4D3/LuaSnip',
+        }
+    }
 })
 
 require("will-x86")
