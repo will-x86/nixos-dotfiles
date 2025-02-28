4c36904fbab4818db0c38166ee912f1c17ac4b34
will-x86 <will@kimbell.uk>
2025-02-28 16:21:36 +0000

    513   2025-02-28 16:20:11   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/platformio.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/platformio.lua
@@ -1,9 +1,11 @@
 return {
-    "anurag3301/nvim-platformio.lua",
+    --[["anurag3301/nvim-platformio.lua",
     dependencies = {
         { "akinsho/nvim-toggleterm.lua" },
         { "nvim-telescope/telescope.nvim" },
         { "nvim-lua/plenary.nvim" },
     },
+    ]] --
+    "normen/vim-pio"
 
 }
