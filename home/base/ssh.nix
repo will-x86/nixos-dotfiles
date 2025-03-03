889ddd24cd3ebf8095e3f0a0a9f93fedcbc93e19
will-x86 <will@kimbell.uk>
2025-03-03 10:13:26 +0000

    523   2025-03-03 10:03:53   (current)

--- home/base/ssh.nix
+++ home/base/ssh.nix
@@ -1,5 +1,6 @@
 {
   config,
+  secrets,
   pkgs,
   ...
 }: {
@@ -7,5 +8,7 @@
   programs.ssh.extraConfig = ''
     Host *
       IdentityFile ~/.ssh/ed25519
+    Host ${secrets.b.a}
+      IdentityFile ~/.ssh/${secrets.b.aq}
   '';
 }
