9b21b5d714d0ef15d2de1fe6a7ed4616d31b4d59
will-x86 <will@kimbell.uk>
2025-03-05 19:14:24 +0000

    536   2025-03-05 11:27:43   (current)

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -28,7 +28,12 @@
       password = "${secrets.syncthing.pass}";
     };
   };
-
+programs.steam = {
+  enable = true;
+  #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+};
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
