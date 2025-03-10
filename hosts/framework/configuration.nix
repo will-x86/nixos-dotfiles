82fe6d45eaf3885c6cf1bb7320550d6a664ca1b8
will-x86 <will@kimbell.uk>
2025-03-10 14:27:02 +0000

    543   2025-03-08 17:23:02   (current)

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -50,13 +50,6 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    #driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      rocmPackages_5.clr.icd
-      rocmPackages_5.clr
-      rocmPackages_5.rocminfo
-      rocmPackages_5.rocm-runtime
-    ];
   };
   hardware.amdgpu.amdvlk = {
     enable = true;
