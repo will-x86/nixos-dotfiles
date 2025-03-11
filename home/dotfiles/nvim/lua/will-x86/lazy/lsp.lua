ca7931fa7b4197fe6dfc873075ac03fa30b08422
will-x86 <will@kimbell.uk>
2025-03-11 10:52:50 +0000

    558   2025-03-11 10:47:34   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
@@ -22,6 +22,7 @@ return {
         local lsp = require("lsp-zero")
         local cmp = require('cmp')
         local cmp_action = lsp.cmp_action()
+        local eslint = require("eslint");
 
 
         require("mason").setup({
@@ -85,6 +86,26 @@ return {
                     })
                 end
             }
+        })
+
+        eslint.setup({
+            bin = 'eslint_d',
+            code_actions = {
+                enable = true,
+                apply_on_save = {
+                    enable = true,
+                    types = { "directive", "problem", "suggestion", "layout" },
+                },
+                disable_rule_comment = {
+                    enable = true,
+                    location = "separate_line", -- or `same_line`
+                },
+            },
+            diagnostics = {
+                enable = true,
+                report_unused_disable_directives = false,
+                run_on = "type",
+            },
         })
         require('java').setup()
 
