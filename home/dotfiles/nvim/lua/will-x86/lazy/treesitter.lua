a7d73b1f647b51842548d073532a69546c362497
will-x86 <will@kimbell.uk>
2025-03-11 11:23:04 +0000

    565   2025-03-11 11:05:49   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/treesitter.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/treesitter.lua
@@ -3,21 +3,48 @@ return {
     build = ':TSUpdate',
     config = function()
         require 'nvim-treesitter.configs'.setup {
-            ensure_installed = { "javascript", "typescript", "c", "lua", "vim" },
-
+            ensure_installed = {
+                "javascript",
+                "typescript",
+                "tsx", -- Add this
+                "jsx", -- Add this
+                "c",
+                "lua",
+                "vim"
+            },
             sync_install = false,
-
             auto_install = true,
-
-            ignore_install = { "javascript" },
-
-
+            -- Remove this line as it's preventing JavaScript parser installation
+            -- ignore_install = { "javascript" },
             highlight = {
                 enable = true,
+                disable = function(lang, buf)
+                    if lang == "html" then
+                        print("disabled")
+                        return true
+                    end
+
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        vim.notify(
+                            "File larger than 100KB treesitter disabled for performance",
+                            vim.log.levels.WARN,
+                            { title = "Treesitter" }
+                        )
+                        return true
+                    end
+                end,
                 additional_vim_regex_highlighting = false,
+            },
+            -- Add this section
+            autotag = {
+                enable = true,
+                enable_rename = true,
+                enable_close = true,
+                enable_close_on_slash = true,
             },
         }
     end
-
-
 }
+
