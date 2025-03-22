42143f35a5fccf92c6ce5b561477615b811d9dab
will-x86 <will@kimbell.uk>
2025-03-22 20:12:32 +0000

    596   2025-03-22 16:55:44   (current)

--- home/base/ssh.nix
+++ home/base/ssh.nix
@@ -6,8 +6,10 @@
 }: {
   programs.ssh.enable = true;
   programs.ssh.extraConfig = ''
-    Host *
+    Host github.com
       IdentityFile ~/.ssh/ed25519
+    Host *
+      IdentityFile ~/.ssh/id_ecdsa_sk
     Host ${secrets.b.a}
       IdentityFile ~/.ssh/${secrets.b.aq}
   '';
