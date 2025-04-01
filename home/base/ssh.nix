ef9148da7743e2f22eee3ffb659093c3180d49c4
will-x86 <will@kimbell.uk>
2025-04-01 11:52:40 +0100

    605   2025-04-01 10:50:18   (current)

--- home/base/ssh.nix
+++ home/base/ssh.nix
@@ -10,6 +10,8 @@
       IdentityFile ~/.ssh/ed25519
     Host *
       IdentityFile ~/.ssh/id_ecdsa_sk
+    Host localhost
+        UserKnownHostsFile /dev/null
     Host ${secrets.b.a}
       IdentityFile ~/.ssh/${secrets.b.aq}
   '';
