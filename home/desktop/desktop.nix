b3e2b69ba9300e5e1c124e0d6967ea8d776fa058
will-x86 <will@kimbell.uk>
2025-04-11 11:37:17 +0100

    640   2025-04-11 11:15:09   (current)

--- home/desktop/desktop.nix
+++ home/desktop/desktop.nix
@@ -23,7 +23,15 @@ in {
   };
   home.packages = with pkgs; [
     pulsemixer
-    bambu-studio
+    bambu-studio = prev.bambu-studio.overrideAttrs (oldAttrs: {
+version = “01.00.01.50”;
+src = prev.fetchFromGitHub {
+owner = “bambulab”;
+repo = “BambuStudio”;
+rev = “v01.00.01.50”;
+hash = “sha256-7mkrPl2CQSfc1lRjl1ilwxdYcK5iRU//QGKmdCicK30=”;
+};
+});
     ventoy-full
     cloudflared
     pkgs-stable.google-chrome
