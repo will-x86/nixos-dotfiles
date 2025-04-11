028fb980c01365613b78338781b6a9e01341f7be
will-x86 <will@kimbell.uk>
2025-04-11 18:43:59 +0100

    646   2025-04-11 18:43:02   (current)

--- flake.nix
+++ flake.nix
@@ -47,24 +47,6 @@
       };
 
       nixosConfigurations = {
-        prodesk = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/all.nix
-            ./hosts/prodesk/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {inherit secrets inputs system;};
-
-              home-manager.users.will = import ./home/base/base.nix;
-            }
-            {
-              _module.args.secrets = secrets;
-            }
-          ];
-        };
         framework = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
