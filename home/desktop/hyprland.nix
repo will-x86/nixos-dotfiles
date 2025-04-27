99c8f55392353e5474175f137adbc330019c917d
will-x86 <will@kimbell.uk>
2025-04-27 23:03:29 +0100

    Sleepy time

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -31,31 +31,31 @@
                   bind=,escape,submap,reset
                   submap=reset
                   decoration {
-        rounding = 6
-        active_opacity = 1
-        inactive_opacity = 0.9
+                    rounding = 6
+                    active_opacity = 1
+                    inactive_opacity = 0.9
 
-          blur {
-            enabled = true
-            size = 5
-            passes = 2
-            ignore_opacity = true
-            popups = true
-            new_optimizations = true
-            noise = 0.0200
-            contrast = 1
-            brightness = 0.8172
-            vibrancy = 0.1696
-          }
+                      blur {
+                        enabled = true
+                        size = 5
+                        passes = 2
+                        ignore_opacity = true
+                        popups = true
+                        new_optimizations = true
+                        noise = 0.0200
+                        contrast = 1
+                        brightness = 0.8172
+                        vibrancy = 0.1696
+                      }
 
-        shadow {
-          enabled = true
-          range = 20
-          render_power = 4
-          color = rgba(000000b3)
-          ignore_window = true
-        }
-      }
+                    shadow {
+                      enabled = true
+                      range = 20
+                      render_power = 4
+                      color = rgba(000000b3)
+                      ignore_window = true
+                    }
+                  }
 
                   animations {
                   	enabled = true
