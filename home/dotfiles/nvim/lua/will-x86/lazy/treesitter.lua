f2fadca20bac1f626ab7ec97e50ffd4f798403b0
will-x86 <will@kimbell.uk>
2025-05-02 17:38:21 +0100

    853   2025-05-02 17:26:43   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/treesitter.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/treesitter.lua
@@ -4,13 +4,8 @@ return {
     config = function()
         require 'nvim-treesitter.configs'.setup {
             ensure_installed = { "javascript", "typescript", "c", "lua", "vim" },
-
             sync_install = false,
-
             auto_install = true,
-
-
-
             highlight = {
                 enable = true,
                 disable = function(lang, buf)
