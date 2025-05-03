b647175686e979909d825df3eb9786aec3671ce0
will-x86 <will@kimbell.uk>
2025-05-03 11:38:03 +0100

    870   2025-05-03 11:36:11   (current)

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -1,7 +1,3 @@
 vim.g.mapleader = " "
 
 require("will-x86")
-if vim.env.TERM == 'xterm-kitty' then
-  vim.cmd([[autocmd UIEnter * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[>1u") | endif]])
-  vim.cmd([[autocmd UILeave * if v:event.chan ==# 0 | call chansend(v:stderr, "\x1b[<1u") | endif]])
-end
--- home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
@@ -98,7 +98,7 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-i>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 --[[['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
