81ffbf6bf5b703f91de5a43ea42e3532350e375f
will-x86 <will@kimbell.uk>
2025-05-05 11:48:47 +0100

    901   2025-05-04 13:09:59   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
@@ -38,6 +38,32 @@ return {
 				require("conform").format({ bufnr = args.buf, async = true })
 			end,
 		})
+		-- Define disabled filetypes
+		local disable_lsp_filetypes = {}
+
+		-- LSP Keybindings and on_attach function
+		local on_attach = function(client, bufnr)
+			local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
+
+			if disable_lsp_filetypes[filetype] then
+				client.stop()
+				return
+			end
+			
+			local opts = { buffer = bufnr, remap = false }
+			vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+			vim.keymap.set("n", "<C-o>", [[<Cmd>lua vim.cmd('normal! <C-O>')<CR>]], opts)
+			vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+			vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+			vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+			vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+			vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+			vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+			vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+			vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+			vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+		end
+
 		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
 		local capabilities = vim.tbl_deep_extend(
@@ -52,6 +78,7 @@ return {
 		-- TypeScript
 		lspconfig.ts_ls.setup({
 			capabilities = capabilities,
+			on_attach = on_attach,
 			init_options = { hostInfo = "neovim" },
 			cmd = { "typescript-language-server", "--stdio" },
 			filetypes = {
@@ -68,6 +95,7 @@ return {
 		-- Go
 		lspconfig.gopls.setup({
 			capabilities = capabilities,
+			on_attach = on_attach,
 			settings = {
 				gopls = {
 					usePlaceholders = true, -- enables parameter completion
