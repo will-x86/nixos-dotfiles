da5aebe10799e194a55833ca58bb3db73d7cc8dc
will-x86 <will@kimbell.uk>
2025-05-05 16:35:39 +0100

    906   2025-05-05 16:20:45   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/telescope.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/telescope.lua
@@ -5,7 +5,13 @@ return {
         "nvim-lua/plenary.nvim"
     },
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            pickers = {
+                find_files = {
+                    hidden = true
+                }
+            },
+        })
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
