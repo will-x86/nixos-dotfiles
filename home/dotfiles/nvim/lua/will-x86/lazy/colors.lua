522b5f889d75b13f77160272c30f8746c1369108
will-x86 <will@kimbell.uk>
2025-05-19 15:57:15 +0100

    945   2025-05-19 14:51:21   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/colors.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/colors.lua
@@ -1,5 +1,5 @@
 return {
-    {
+	--[[{
         --"zenbones-theme/zenbones.nvim",
         "arcticicestudio/nord-vim",
         dependencies = "rktjmp/lush.nvim",
@@ -10,16 +10,29 @@ return {
             vim.opt.background = "dark"
             vim.cmd("colorscheme nord")
         end
-    },
-    {
-        'xiyaowong/transparent.nvim',
-        config = function()
-            require("transparent").setup({
-                extra_groups = {
-                    "NormalFloat"
-                },
-            })
-            vim.cmd("TransparentEnable")
-        end
-    },
+    },]]
+	--
+	{
+		"oxfist/night-owl.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			require("night-owl").setup()
+			vim.o.termguicolors = true
+			vim.opt.background = "dark"
+			vim.cmd.colorscheme("night-owl")
+		end,
+	},
+	{
+		"xiyaowong/transparent.nvim",
+		config = function()
+			require("transparent").setup({
+				extra_groups = {
+					"NormalFloat",
+				},
+			})
+			vim.cmd("TransparentEnable")
+		end,
+	},
 }
