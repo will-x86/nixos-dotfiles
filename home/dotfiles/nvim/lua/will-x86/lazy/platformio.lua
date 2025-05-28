25a1336da4a768c10e3ecc6c1b9c50d40174e54d
will-x86 <will@kimbell.uk>
2025-05-28 17:56:27 +0100

    980   2025-05-28 17:52:42   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/platformio.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/platformio.lua
@@ -1,9 +1,13 @@
 return {
-    "anurag3301/nvim-platformio.lua",
-    dependencies = {
-        { "akinsho/nvim-toggleterm.lua" },
-        { "nvim-telescope/telescope.nvim" },
-        { "nvim-lua/plenary.nvim" },
-    },
-
+	"anurag3301/nvim-platformio.lua",
+	dependencies = {
+		{ "akinsho/nvim-toggleterm.lua" },
+		{ "nvim-telescope/telescope.nvim" },
+		{ "nvim-lua/plenary.nvim" },
+	},
+	config = function()
+		require("platformio").setup({
+			lsp = "clangd",
+		})
+	end,
 }
