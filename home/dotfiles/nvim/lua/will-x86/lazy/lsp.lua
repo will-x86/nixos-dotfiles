519a35b9de4b1617c257f9c550e071aec7834c6c
will-x86 <will@kimbell.uk>
2025-06-10 11:50:44 +0100

    1002   2025-06-10 11:49:10   (current)

--- home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
+++ home/dotfiles/nvim/lua/will-x86/lazy/lsp.lua
@@ -10,6 +10,12 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+         opts = function(_, opts)
+      local esp32 = require("esp32")
+      opts.servers = opts.servers or {}
+      opts.servers.clangd = esp32.lsp_config()
+      return opts
+    end,
 		dependencies = {
 			"stevearc/conform.nvim",
 			"Aietes/esp32.nvim",
