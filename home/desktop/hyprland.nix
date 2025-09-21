1e29b56c9e5863f0f784a48f8b88976d6cb415b5
will-x86 <will@kimbell.uk>
2025-09-21 10:42:39 +0100

    1193   2025-09-20 09:36:10   (current)

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -115,8 +115,6 @@
       }
 
       gestures {
-      	workspace_swipe = true
-      	workspace_swipe_fingers = 3
       	workspace_swipe_distance = 300
       	workspace_swipe_invert = true
       	workspace_swipe_min_speed_to_force = 30
