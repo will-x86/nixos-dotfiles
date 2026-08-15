9e1a93728163074a6b0ddcc74730ec0b73bbaf8c
will-x86 <will@kimbell.uk>
2026-08-15 13:11:28 +0100

    1427   2026-08-15 13:03:28   (current)

--- home/base/nixvim/default.nix
+++ home/base/nixvim/default.nix
@@ -37,6 +37,7 @@
       python312Packages.pylatexenc
     ];
     plugins = {
+      figet.enable = true;
       image.enable = true;
       #bufferline.enable = true;
       web-devicons.enable = true;
