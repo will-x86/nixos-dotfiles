dec80d663fb6d64c5457682f98db25b34a37dd34
will-x86 <will@kimbell.uk>
2026-08-15 14:17:58 +0100

    1431   2026-08-15 14:16:12   (current)

--- home/base/nixvim/default.nix
+++ home/base/nixvim/default.nix
@@ -71,8 +71,13 @@
     };
     extraConfigLua = ''
       vim.g.vimtex_compiler_latexmk = { out_dir = "build" }
-      -- Make inlay hints more readable
-      vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#88c0d0", bg = "NONE" })
+      -- Make inlay hints more readable (apply after colorscheme loads)
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#88c0d0", bg = "NONE" })
+        end,
+      })
     '';
   };
 }
