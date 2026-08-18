30cf64c3a44750ccd240c3d954209873328cef76
will-x86 <will@kimbell.uk>
2026-08-18 12:06:13 +0100

    1438   2026-08-17 13:00:05   (current)

--- home/base/programming.nix
+++ home/base/programming.nix
@@ -4,40 +4,41 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
-    # nix-specific
-    nixos-shell # vm's
-    nixos-generators
+  home.packages =
+    (with pkgs; [
+      # nix-specific
+      nixos-shell # vm's
+      nixos-generators
 
-    devenv
-    # Languages
-    typescript # nixvim
-    go
-    gcc
-    nodejs
-    python3
-    ccls
-    jdk17
-    # lsp stuff
-    typescript-language-server # nixvim
-    astro-language-server # zed .. ?
-    black
-    eslint
-    nixd
-    # extensions / other
-    maven
-    uv
-    rust-analyzer
-    yarn
-    cargo
-    openssl
-    pkg-config
-    python312Packages.dbus-python
-    # tui programing
-    opencode
-  ];
-  home.packages = with pkgs-stable; [
-    python312Packages.marimo
+      devenv
+      # Languages
+      typescript # nixvim
+      go
+      gcc
+      nodejs
+      python3
+      ccls
+      jdk17
+      # lsp stuff
+      typescript-language-server # nixvim
+      astro-language-server # zed .. ?
+      black
+      eslint
+      nixd
+      # extensions / other
+      maven
+      uv
+      rust-analyzer
+      yarn
+      cargo
+      openssl
+      pkg-config
+      python312Packages.dbus-python
+      # tui programing
+      opencode
+    ])
+    ++ (with pkgs-stable; [
+      python312Packages.marimo
+    ]);
 
-  ];
 }
