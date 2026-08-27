53865dbc313a1b912d20491cb6c6154916c1166e
will-x86 <will@kimbell.uk>
2026-08-27 13:04:07 +0100

    1439   2026-08-27 12:46:00   (current)

--- home/desktop/gui.nix
+++ home/desktop/gui.nix
@@ -1,32 +1,36 @@
 {
   pkgs,
+  pkgs-stable,
   ...
 }:
 {
-  home.packages = with pkgs; [
-    blender
-    obs-studio
-    mediawriter # fedoras media writer
-    trayscale
-    libreoffice
-    discord
-    zoom-us
-    kicad
-    kdePackages.dolphin
-    #kdePackages.dolphin
-    #kdePackages.dolphin-plugins
-    #kdePackages.baloo-widgets
-    #kdePackages.baloo
-    freecad
+  home.packages =
+    (with pkgs; [
+      blender
+      obs-studio
+      mediawriter # fedoras media writer
+      trayscale
+      libreoffice
+      discord
+      zoom-us
+      kicad
+      kdePackages.dolphin
+      #kdePackages.dolphin
+      #kdePackages.dolphin-plugins
+      #kdePackages.baloo-widgets
+      #kdePackages.baloo
+      freecad
 
-    obsidian
-    spotify
-    chromium
-    anydesk
-    davinci-resolve
-    #celeste
-    signal-desktop
-    kdePackages.okular # pdf
+      obsidian
+      spotify
+      chromium
+      anydesk
+      davinci-resolve
+      signal-desktop
+      kdePackages.okular # pdf
 
-  ];
+    ])
+    ++ (with pkgs-stable; [
+      celeste
+    ]);
 }
