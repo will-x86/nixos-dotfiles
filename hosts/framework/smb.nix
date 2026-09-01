74777685d97827e1645f463a26d560cb2718a5c1
will-x86 <will@kimbell.uk>
2026-09-01 12:54:58 +0100

    1475   2026-09-01 12:54:34   (current)

--- hosts/framework/smb.nix
+++ hosts/framework/smb.nix
@@ -1,20 +1,5 @@
 { secrets, ... }:
 {
-  fileSystems."/mnt/Immich" = {
-    device = "${secrets.samba.fracRemote}:/mnt/Vault/Immich";
-    fsType = "nfs";
-    options = [
-      "x-systemd.automount"
-      "noauto" # Will mount first time accessed
-      "x-systemd.idle-timeout=600" # diss after 600s
-      "noatime"
-      "rw"
-      "soft"
-      "nfsvers=4.2"
-      "async"
-      "nofail"
-    ];
-  };
   fileSystems."/mnt/FractalMedia" = {
     device = "${secrets.samba.fracRemote}:/mnt/Vault/Media";
     fsType = "nfs";
