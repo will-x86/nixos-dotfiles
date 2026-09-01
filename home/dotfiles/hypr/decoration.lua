ca3033c0fa4764ad2f265d5420f7cec1ea84aec5
will-x86 <will@kimbell.uk>
2026-09-01 13:03:02 +0100

    1478   2026-09-01 13:01:16   (current)

--- home/dotfiles/hypr/decoration.lua
+++ home/dotfiles/hypr/decoration.lua
@@ -1,28 +1,55 @@
+--
+-- Look and feel configuration
+CACHYLGREEN = "rgba(82dcccff)"
+CACHYMGREEN = "rgba(00aa84ff)"
+CACHYDGREEN = "rgba(007d6fff)"
+CACHYLBLUE = "rgba(01ccffff)"
+CACHYMBLUE = "rgba(182545ff)"
+CACHYDBLUE = "rgba(111826ff)"
+CACHYWHITE = "rgba(ffffffff)"
+CACHYGREY = "rgba(ddddddff)"
+CACHYGRAY = "rgba(798bb2ff)"
 hl.config({
-	decoration = {
-		rounding = 6,
-		active_opacity = 1.0,
-		inactive_opacity = 0.9,
-
-		blur = {
-			enabled = true,
-			size = 4,
-			passes = 2,
-			ignore_opacity = true,
-			popups = true,
-			new_optimizations = true,
-			noise = 0.0200,
-			contrast = 1,
-			brightness = 0.8172,
-			vibrancy = 0.1696,
+	general = {
+		gaps_in = 3,
+		gaps_out = 8,
+		border_size = 2,
+		extend_border_grab_area = 10,
+		resize_on_border = true,
+		col = {
+			active_border = {
+				colors = { CACHYLGREEN, CACHYDGREEN },
+				angle = 45,
+			},
+			inactive_border = CACHYGRAY,
 		},
-
-		shadow = {
-			enabled = true,
-			range = 20,
-			render_power = 4,
-			color = "rgba(000000b3)",
-			--ignore_window = true,
+	},
+	group = {
+		col = {
+			border_active = CACHYLBLUE,
+			border_inactive = CACHYGRAY,
+			border_locked_active = CACHYDBLUE,
+			border_locked_inactive = CACHYGRAY,
+		},
+		groupbar = {
+			col = {
+				active = CACHYLGREEN,
+				inactive = CACHYGRAY,
+				locked_active = CACHYDBLUE,
+				locked_inactive = CACHYGRAY,
+			},
+		},
+	},
+	decoration = {
+		dim_special = 0.3,
+		rounding = 10,
+		active_opacity = 0.95,
+		inactive_opacity = 0.85,
+		fullscreen_opacity = 1,
+		blur = {
+			size = 5,
+			passes = 4,
+			special = true,
 		},
 	},
 })
